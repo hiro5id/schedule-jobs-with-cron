2c08103520a74f75ff409bade017e74bea4521cd
Roberto Sebestyen <roberto@sebestyen.ca>
2023-01-06 17:51:56 -0500

    add additional test cases

--- test/cron-scheduler.test.ts
+++ test/cron-scheduler.test.ts
@@ -57,7 +57,7 @@ describe('chron scheduler', function () {
     expect(triggerTwoReached).to.be.true;
   });
 
-  it('schedules async job', async function () {
+  it('schedules a-sync job', async function () {
     let mockDate = `2010-01-01T00:00:00.000Z`;
     const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
     jobGetNowMock.mockImplementation(() => {
@@ -70,13 +70,16 @@ describe('chron scheduler', function () {
     let triggerTwoReached = false;
 
     const jobWorkerFunction: JobWorkerFunction = async (triggerTime: Date, log: LoggerFunction) => {
-      log('info', `Hello this is a test ${triggerTime}`);
-      if (!triggerOneReached) {
-        triggerOneReached = true;
-      }
-      if (!triggerTwoReached) {
-        triggerTwoReached = true;
-      }
+      return new Promise(resolve => {
+        log('info', `Hello this is a test ${triggerTime}`);
+        if (!triggerOneReached) {
+          triggerOneReached = true;
+        }
+        if (!triggerTwoReached) {
+          triggerTwoReached = true;
+        }
+        resolve();
+      });
     };
 
     let afterSettingTimeoutCallbackCount = 0;
@@ -104,7 +107,7 @@ describe('chron scheduler', function () {
     expect(triggerTwoReached).to.be.true;
   });
 
-  it('logs messages for async job', async function () {
+  it('logs messages for a-sync job', async function () {
     let mockDate = `2010-01-01T00:00:00.000Z`;
     const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
     jobGetNowMock.mockImplementation(() => {
@@ -126,7 +129,10 @@ describe('chron scheduler', function () {
     const endTime = new Date(`2010-01-01T00:04:00.000Z`);
 
     const jobWorkerFunction: JobWorkerFunction = async (triggerTime: Date, log: LoggerFunction) => {
-      log('info', `Hello this is a test ${triggerTime}`);
+      return new Promise(resolve => {
+        log('info', `Hello this is a test ${triggerTime}`);
+        resolve();
+      });
     };
 
     let afterSettingTimeoutCallbackCount = 0;
@@ -279,7 +285,7 @@ describe('chron scheduler', function () {
     expect(consoleErrors).eql([]);
   });
 
-  it('throws error', async function () {
+  it('throws error before worker function', async function () {
     let mockDate = `2010-01-01T00:00:00.000Z`;
     const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
     jobGetNowMock.mockImplementation(() => {
@@ -338,5 +344,186 @@ describe('chron scheduler', function () {
     ]);
 
     expect(consoleErrors).eql([]);
+  });
+
+  it('throws error inside sync worker', async function () {
+    let mockDate = `2010-01-01T00:00:00.000Z`;
+    const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
+    jobGetNowMock.mockImplementation(() => {
+      return new Date(mockDate);
+    });
+
+    const consoleMessages: string[] = [];
+    const consoleLogMock = jest.spyOn(console, 'log');
+    consoleLogMock.mockImplementation(msg => {
+      consoleMessages.push(msg);
+    });
+
+    const consoleErrors: string[] = [];
+    const consoleErrMock = jest.spyOn(console, 'error');
+    consoleErrMock.mockImplementation(msg => {
+      consoleErrors.push(msg);
+    });
+
+    const endTime = new Date(`2010-01-01T00:04:00.000Z`);
+
+    const jobWorkerFunction: JobWorkerFunction = (_triggerTime: Date, _log: LoggerFunction) => {
+      throw new Error(`This is an error inside async worker function`);
+    };
+
+    let afterSettingTimeoutCallbackCount = 0;
+
+    const job = new Job('testjob123', jobWorkerFunction, '*/1 * * * *', {
+      endDate: endTime,
+      startDate: new Date(mockDate),
+      continueOnError: false,
+      afterSettingTimeoutCallback: () => {
+        jest.advanceTimersByTime(60000);
+      },
+      beforeExecutingWorkerCallback: () => {
+        afterSettingTimeoutCallbackCount += 1;
+        mockDate = `2010-01-01T00:0${afterSettingTimeoutCallbackCount}:00.000Z`;
+      },
+    });
+
+    let caughtError: string | null = null;
+    try {
+      await job.getPromise();
+    } catch (err) {
+      caughtError = err as string;
+    }
+
+    expect(caughtError).eql(
+      'Job [testjob123]: Error running job at iteration Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time): Job [testjob123]: Error: This is an error inside async worker function',
+    );
+
+    expect(consoleMessages).eql([
+      'Job [testjob123]: Scheduled to execute: At every minute',
+      'Job [testjob123]: Scheduling to trigger in the next 60000 ms, at (Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time)) the time is now Thu Dec 31 2009 19:00:00 GMT-0500 (Eastern Standard Time).',
+    ]);
+
+    expect(consoleErrors).eql(['Job [testjob123]: Failed to execute, following error was received: Error: This is an error inside async worker function']);
+  });
+
+  it('throws error inside a-sync worker', async function () {
+    let mockDate = `2010-01-01T00:00:00.000Z`;
+    const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
+    jobGetNowMock.mockImplementation(() => {
+      return new Date(mockDate);
+    });
+
+    const consoleMessages: string[] = [];
+    const consoleLogMock = jest.spyOn(console, 'log');
+    consoleLogMock.mockImplementation(msg => {
+      consoleMessages.push(msg);
+    });
+
+    const consoleErrors: string[] = [];
+    const consoleErrMock = jest.spyOn(console, 'error');
+    consoleErrMock.mockImplementation(msg => {
+      consoleErrors.push(msg);
+    });
+
+    const endTime = new Date(`2010-01-01T00:04:00.000Z`);
+
+    const jobWorkerFunction: JobWorkerFunction = async (_triggerTime: Date, _log: LoggerFunction) => {
+      return new Promise(() => {
+        throw new Error(`This is an error inside async worker function`);
+      });
+    };
+
+    let afterSettingTimeoutCallbackCount = 0;
+
+    const job = new Job('testjob123', jobWorkerFunction, '*/1 * * * *', {
+      endDate: endTime,
+      startDate: new Date(mockDate),
+      continueOnError: false,
+      afterSettingTimeoutCallback: () => {
+        jest.advanceTimersByTime(60000);
+      },
+      beforeExecutingWorkerCallback: () => {
+        afterSettingTimeoutCallbackCount += 1;
+        mockDate = `2010-01-01T00:0${afterSettingTimeoutCallbackCount}:00.000Z`;
+      },
+    });
+
+    let caughtError: string | null = null;
+    try {
+      await job.getPromise();
+    } catch (err) {
+      caughtError = err as string;
+    }
+
+    expect(caughtError).eql(
+      'Job [testjob123]: Error running job at iteration Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time): Job [testjob123]: Error: This is an error inside async worker function',
+    );
+
+    expect(consoleMessages).eql([
+      'Job [testjob123]: Scheduled to execute: At every minute',
+      'Job [testjob123]: Scheduling to trigger in the next 60000 ms, at (Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time)) the time is now Thu Dec 31 2009 19:00:00 GMT-0500 (Eastern Standard Time).',
+    ]);
+
+    expect(consoleErrors).eql(['Job [testjob123]: Failed to execute, following error was received: Error: This is an error inside async worker function']);
+  });
+
+  it('rejects inside a-sync worker', async function () {
+    let mockDate = `2010-01-01T00:00:00.000Z`;
+    const jobGetNowMock = jest.spyOn(Job.prototype as any, 'getNow');
+    jobGetNowMock.mockImplementation(() => {
+      return new Date(mockDate);
+    });
+
+    const consoleMessages: string[] = [];
+    const consoleLogMock = jest.spyOn(console, 'log');
+    consoleLogMock.mockImplementation(msg => {
+      consoleMessages.push(msg);
+    });
+
+    const consoleErrors: string[] = [];
+    const consoleErrMock = jest.spyOn(console, 'error');
+    consoleErrMock.mockImplementation(msg => {
+      consoleErrors.push(msg);
+    });
+
+    const endTime = new Date(`2010-01-01T00:04:00.000Z`);
+
+    const jobWorkerFunction: JobWorkerFunction = async (_triggerTime: Date, _log: LoggerFunction) => {
+      return new Promise((_resolve, reject) => {
+        reject('we are rejecting from inside jobWorkerFunction');
+      });
+    };
+
+    let afterSettingTimeoutCallbackCount = 0;
+
+    const job = new Job('testjob123', jobWorkerFunction, '*/1 * * * *', {
+      endDate: endTime,
+      startDate: new Date(mockDate),
+      continueOnError: false,
+      afterSettingTimeoutCallback: () => {
+        jest.advanceTimersByTime(60000);
+      },
+      beforeExecutingWorkerCallback: () => {
+        afterSettingTimeoutCallbackCount += 1;
+        mockDate = `2010-01-01T00:0${afterSettingTimeoutCallbackCount}:00.000Z`;
+      },
+    });
+
+    let caughtError: string | null = null;
+    try {
+      await job.getPromise();
+    } catch (err) {
+      caughtError = err as string;
+    }
+
+    expect(caughtError).eql(
+      'Job [testjob123]: Error running job at iteration Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time): Job [testjob123]: we are rejecting from inside jobWorkerFunction',
+    );
+
+    expect(consoleMessages).eql([
+      'Job [testjob123]: Scheduled to execute: At every minute',
+      'Job [testjob123]: Scheduling to trigger in the next 60000 ms, at (Thu Dec 31 2009 19:01:00 GMT-0500 (Eastern Standard Time)) the time is now Thu Dec 31 2009 19:00:00 GMT-0500 (Eastern Standard Time).',
+    ]);
+
+    expect(consoleErrors).eql(['Job [testjob123]: Failed to execute, following error was received: we are rejecting from inside jobWorkerFunction']);
   });
 });
